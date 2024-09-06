720bdc9dd5b437e2aec39ff2d3d6f68bbd3ade16
KalyanRamagiri <kalyanr458@gmail.com>
2024-09-06 08:21:28 +0530

    First Commit and push

---
+++ src/sept/ex_06092024/Lab002.java
@@ -0,0 +1,13 @@
+package sept.ex_06092024;
+
+public class Lab002 {
+    public static void main (String[] args){
+        System.out.println("Hello ATB8x");
+        //Above output will be displayed
+        /*
+        Author: Kalyan Ramagiri
+        Course from : ATB8x thetestingAcademy
+        Multiline commments
+         */
+    }
+}
